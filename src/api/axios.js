a5da7d0012c33c09ad7083e4b559ca4310ae5e72
Abdul Muez <mmuez58@gmail.com>
2026-02-12 22:06:03 +0530

    Update baseURL and push production changes

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,5 +1,6 @@
 import axios from 'axios'
 
+// const api = axios.create({ baseURL: "http://localhost:3000" })
 const api = axios.create({ baseURL: "https://playlist-backend-ssro.onrender.com" })
 
 api.interceptors.request.use((config) => {
