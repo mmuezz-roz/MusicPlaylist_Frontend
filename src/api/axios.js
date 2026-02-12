7ab9b6a18e72a56151329503285713067853b890
Abdul Muez <mmuez58@gmail.com>
2026-02-12 22:04:03 +0530

    Point frontend to production backend URL

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,11 +1,11 @@
 import axios from 'axios'
 
-const api = axios.create({baseURL:"http://localhost:3000"})
+const api = axios.create({ baseURL: "https://playlist-backend-ssro.onrender.com" })
 
 api.interceptors.request.use((config) => {
     const token = localStorage.getItem("token")
 
-    if(token){
+    if (token) {
         config.headers.Authorization = `Bearer ${token}`
     }
     return config
