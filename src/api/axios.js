708f5ce902ede63bfe103ba743f5b4e8c921cb4c
Abdul Muez <mmuez58@gmail.com>
2026-02-13 23:19:21 +0530

    finalized responsive frontend

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,10 +1,10 @@
 import axios from 'axios'
 
-const api = axios.create({ baseURL: "http://localhost:3000" })
-// const api = axios.create({
-//     baseURL: "https://melodyhub-backend.vercel.app",
-//     withCredentials: true
-// })
+// const api = axios.create({ baseURL: "http://localhost:3000" })
+const api = axios.create({
+    baseURL: "https://melodyhub-backend.vercel.app",
+    withCredentials: true
+})
 
 api.interceptors.request.use((config) => {
     const token = localStorage.getItem("token")
