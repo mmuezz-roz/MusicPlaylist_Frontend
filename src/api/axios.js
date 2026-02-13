073a06f9c1830a89e0a55ef2efbebdbb94c80e53
Abdul Muez <mmuez58@gmail.com>
2026-02-13 20:05:07 +0530

    Update axios configuration

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 // const api = axios.create({ baseURL: "http://localhost:3000" })
-const api = axios.create({ baseURL: "https://playlist-backend-ssro.onrender.com" })
+const api = axios.create({ baseURL: "https://melodyhub-backend.vercel.app" })
 
 api.interceptors.request.use((config) => {
     const token = localStorage.getItem("token")
